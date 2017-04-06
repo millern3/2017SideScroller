fec16894c8a070e40f9e80cf7e576ad69dca8555
Unknown <millern2@hawkmail.newpaltz.edu>
2017-04-06 10:11:25 -0400

    Script and Game over

--- Assets/Scripts/GM.cs
+++ Assets/Scripts/GM.cs
@@ -6,11 +6,10 @@ using UnityEngine.UI;
 public class GM : MonoBehaviour {
     private int _Lives = 3;
     public int points;
-    public GameObject gameOver; 
+    public GameObject gameOverSign; 
     public GameObject youWin;
     public Text livesValue;
     public Text pointsValue;
-    public float deadZone = -3.2f;
 
     
     public void SetLives(int newValue)
@@ -18,38 +17,19 @@ public class GM : MonoBehaviour {
         _Lives = newValue;
         Debug.Log("Lives now equals:" + _Lives);
         livesValue.text = _Lives.ToString();
+
+        if(_Lives == 0)
+        {
+            gameOverSign.SetActive(true);
+        }
     }
     public int GetLives()
     {
         return _Lives;
     }
-    void Update ()
-    {
-        if(transform.position.y < deadZone)
-        {
-            GetOut();
-        }
-    }
-    void GetOut ()
-    {
-        if(_Lives == 0)
-        {
-            DoGameOver();
-        }
-    }
-    void DoGameOver()
-    {
-        gameOver.SetActive(true);
-    }
     public void CoinCollected (int worth)
     {
         points += worth;
         pointsValue.text = points.ToString();
-
-        var coinsLeft = FindObjectsOfType<Coin>().Length;
-        if(coinsLeft == 0)
-        {
-            youWin.SetActive(true);
-        } 
     }
 }
