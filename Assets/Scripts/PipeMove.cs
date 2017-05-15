1cd827f58a5046c8bca343fa839bfc83231201f7
Unknown <millern2@hawkmail.newpaltz.edu>
2017-05-15 12:01:52 -0400

    Final: PipeMoveMario

---
+++ Assets/Scripts/PipeMove.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeMove : MonoBehaviour {
+
+    public GameObject ReLocate;
+    public Player player;
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (enabled)
+        {
+            player.transform.position = ReLocate.transform.position;
+        }
+
+    }
+
+}
